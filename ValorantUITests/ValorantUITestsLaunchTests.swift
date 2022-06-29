fcd7aac9b2ecd3a1ff9bff98ba31c1530d7fb24e
wspr <wspr@gft.com>
2022-06-29 13:05:13 -0300

    Initial Commit

---
+++ ValorantUITests/ValorantUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ValorantUITestsLaunchTests.swift
+//  ValorantUITests
+//
+//  Created by Pinto Junior, William James on 29/06/22.
+//
+
+import XCTest
+
+class ValorantUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
